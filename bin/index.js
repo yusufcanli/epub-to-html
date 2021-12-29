d1d04ee038a47580709f6fc4b173c5de67b05123
Yusuf Canlı <cdario.done@gmail.com>
2021-12-29 03:40:30 +0300

    Add files via upload

---
+++ bin/index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const StreamZip = require('node-stream-zip');
+const fs = require('fs');
+var path = require('path');
+
+if (process.argv.length < 3) {
+        console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+        process.exit(1);
+    }
+filename = process.argv[2];
+    const zip = new StreamZip({
+        file: filename,
+        storeEntries: true
+    });
+    
+    zip.on('ready', () => {
+        var chps = [];
+        var imgs = [];
+        for (const entry of Object.values(zip.entries())) {
+            let ext = path.extname(entry.name)
+            if (ext == '.html' || ext == '.xhtml' || ext == '.htm') {
+                let text = zip.entryDataSync(entry.name).toString('utf8');
+                chps.push({name:entry.name, text: text})
+            }
+            if (ext == '.jpg' || ext == '.png' || ext == '.jpeg') {
+                let image = zip.entryDataSync(entry.name).toString('base64');
+                imgs.push({name:entry.name, source:image})
+            }
+        }
+        chps.sort((a, b) => a.name > b.name ? 1 : -1);
+        var db = ''
+        chps.map(x => {
+            db += x.text
+        })
+        imgs.map(img => {
+            console.log(img)
+            db = db.split(`${img.name}`).join(`data:image/png;base64, ${img.source}`)
+        })
+        const file = filename.split('.').slice(0, -1).join('.')
+        fs.writeFileSync(`${file}.html`, (db))
+        zip.close()
+    });
+  
